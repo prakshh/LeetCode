e0451aad73987207989f20dbcaa771e3ddf88be0
prakshh <prakashdas18@gmail.com>
2020-11-07 09:37:55 +0530

    oct21-31 commit

---
+++ october/Day_21_735_Asteroid_Collision.java
@@ -0,0 +1,31 @@
+package main.cp.leetcode.october;
+
+public class Day_21_735_Asteroid_Collision {
+    public int[] asteroidCollision(int[] asteroids) {
+        if (asteroids.length <= 1) return asteroids;  // Handling egde cases
+        
+        Stack<Integer> stack = new Stack<>();
+        for (int asteriod : asteroids) {
+            // add to stack all positive asteroids
+            if (asteriod > 0) { 
+                stack.push(asteriod);
+            } else {
+                // Remove all positive asteroids before our current asteroid
+                while (!stack.isEmpty() && stack.peek() > 0 && Math.abs(stack.peek()) < Math.abs(asteriod))  
+                    stack.pop();
+                // Checking if the stack is empty or the recent asteriod is negative!
+                if (stack.isEmpty() || stack.peek() < 0) 
+                    stack.push(asteriod);
+                // If recent asteriod <= our asteriod, We broke our outer loop if equal we pop it.
+                else if (stack.peek() == Math.abs(asteriod)) 
+                    stack.pop();
+            }
+        }
+        
+        int[] arr = new int[stack.size()];
+        for (int i = arr.length - 1; i >= 0; i--)
+            arr[i] = stack.pop();
+        
+        return arr;
+    }
+}
---
+++ october/Day_26_799_Champagne_Tower.java
@@ -0,0 +1,18 @@
+package main.cp.leetcode.october;
+
+public class Day_26_799_Champagne_Tower {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[][] M = new double[101][101];
+        M[0][0] = poured;
+        for(int r = 0; r <= query_row; r++) {
+            for(int c = 0; c <= r; c++) {
+                double of = (M[r][c] - 1.0) / 2.0;
+                if (of > 0) {
+                    M[r + 1][c] += of;
+                    M[r + 1][c + 1] += of;
+                }
+            }
+        }
+        return Math.min(1, M[query_row][query_glass]);
+    }
+}
