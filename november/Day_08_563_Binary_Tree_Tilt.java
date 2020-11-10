09c1de5da5f0747b4e6a29b4b18ae93e1cdc1684
prakshh <prakashdas18@gmail.com>
2020-11-10 11:43:51 +0530

    lc nov8

---
+++ november/Day_08_563_Binary_Tree_Tilt.java
@@ -0,0 +1,23 @@
+package main.cp.leetcode.november;
+
+import main.cp.leetcode.TreeNode;
+
+public class Day_08_563_Binary_Tree_Tilt {
+    class Solution {
+        int result = 0;
+
+        public int findTilt(TreeNode root) {
+            tilt(root);
+            return result;
+        }
+
+        private int tilt(TreeNode root) {
+            if (root == null)
+                return 0;
+            int leftSum = tilt(root.left);
+            int rightSum = tilt(root.right);
+            result += Math.abs(leftSum - rightSum);
+            return leftSum + rightSum + root.val;
+        }
+    }
+}
