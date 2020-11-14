ae32b1eeaca1b363e25bd2e715189019ee4d64f1
prakshh <prakashdas18@gmail.com>
2020-11-14 13:03:41 +0530

    lc Nov day13

---
+++ november/Day_13_116_Populating_Next_Right_Pointers_in_Each_Node.java
@@ -0,0 +1,50 @@
+package main.cp.leetcode.november;
+
+import java.util.*;
+
+public class Day_13_116_Populating_Next_Right_Pointers_in_Each_Node {
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+                Node levelStart = root;
+        while (levelStart != null) {
+            Node curr = levelStart;
+            while (curr != null) {
+                if (curr.left != null) {
+                    curr.left.next = curr.right;
+                }
+                if (curr.right != null && curr.next != null) {
+                    curr.right.next = curr.next.left;
+                }
+                // move the current node to the next position
+                curr = curr.next;
+            }
+            // move the levelStart node the next level's head position
+            levelStart = levelStart.left;
+        }
+        return root;
+    }
+}
