9f3a78436a5141ea72abe0e119d6ae1145f5889f
prakshh <prakashdas18@gmail.com>
2020-12-21 22:21:00 +0530

    day21

---
+++ december2020/Day_21_910_Smallest_Range_II.java
@@ -0,0 +1,18 @@
+public class Day_21_910_Smallest_Range_II {
+class Solution {
+    TC : O(nlogn) SC: O(1)
+    public int smallestRangeII(int[] A, int K) {
+        Arrays.sort(A); // O(nlogn)
+        int len = A.length -1;
+        int lowest = A[0];
+        int highest = A[len];
+        int res = highest - lowest;
+        for(int i=0;i<len;i++){ //O(n)
+            int min = Math.min(lowest + K, A[i+1] -K);
+            int max = Math.max(highest -K, A[i]+K);
+            res =  Math.min(max - min, res);
+        }
+        return res;
+    }
+}
+}
