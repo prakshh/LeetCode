4e981595f0bfbdeebe7240d2a988370d94e564e3
prakshh <prakashdas18@gmail.com>
2021-01-11 20:03:37 +0530

    day6 jan2021

---
+++ _2021/_01_january2021/Day_06_1539_Kth_Missing_Positive_Number.java
@@ -0,0 +1,18 @@
+import main.cp.leetcode.ListNode;
+
+public class Day_06_1539_Kth_Missing_Positive_Number {
+    class Solution {
+        public int findKthPositive(int[] arr, int k) {
+          int l = 0;
+          int r = arr.length;
+          while (l < r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] - (m + 1) >= k)
+              r = m;
+            else
+              l = m + 1;
+          }
+          return l + k;
+        }
+      }
+}
