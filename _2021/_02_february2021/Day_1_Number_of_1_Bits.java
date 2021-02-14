f5dc2c51845875da5d122f1b7987b00ba39f9578
prakshh <prakashdas18@gmail.com>
2021-02-14 22:22:35 +0530

    Feb2021 Day 1

---
+++ _2021/_02_february2021/Day_1_Number_of_1_Bits.java
@@ -0,0 +1,16 @@
+
+public class Day_1_Number_of_1_Bits {
+	public class Solution {
+	    // you need to treat n as an unsigned value
+	    public int hammingWeight(int n) {
+	        int count = 0;
+
+	        for(int i=0;i<32;i++){
+	            if(((n>>i)&1) ==1){
+	                count++;
+	            }
+	        }
+	        return count;
+	    }
+	}
+}
