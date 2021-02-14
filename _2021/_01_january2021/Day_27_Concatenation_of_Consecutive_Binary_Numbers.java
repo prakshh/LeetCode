2a989cf6794f3d84003dd5dfb8d37c96930a2b6b
prakshh <prakashdas18@gmail.com>
2021-02-14 22:03:37 +0530

    Day 27

---
+++ _2021/_01_january2021/Day_27_Concatenation_of_Consecutive_Binary_Numbers.java
@@ -0,0 +1,22 @@
+
+public class Day_27_Concatenation_of_Consecutive_Binary_Numbers {
+	class Solution {
+	    public int concatenatedBinary(int n) {
+	        int mod = 1000000007 ;
+	        int newNo =0;
+
+	        for(int num=1;num<=n;num++){
+
+	            String binaryRep= Integer.toBinaryString(num);
+
+	            for(char ch : binaryRep.toCharArray()){
+	                int val= (ch=='0')?0:1;
+
+	                newNo= ((newNo*2)%mod + val) % mod;
+	            }
+	        }
+
+	        return newNo;
+	    }
+	}
+}
