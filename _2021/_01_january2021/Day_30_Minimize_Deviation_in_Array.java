6805f36b4aa1600cf0f3a19e67cacf5e18fa1d01
prakshh <prakashdas18@gmail.com>
2021-02-14 22:12:31 +0530

    Day 30

---
+++ _2021/_01_january2021/Day_30_Minimize_Deviation_in_Array.java
@@ -0,0 +1,31 @@
+import java.util.TreeSet;
+
+public class Day_30_Minimize_Deviation_in_Array {
+	class Solution {
+	    public int minimumDeviation(int[] nums) {
+	        TreeSet<Integer> set = new TreeSet<>();
+	        for(int el: nums){
+	            if(el%2==1){
+	                set.add(el*2);
+	            } else{
+	                set.add(el);
+	            }
+	        }
+
+	        int diff = Integer.MAX_VALUE;
+	        while(true){
+	            int maxVal = set.last();
+	            int minVal = set.first();
+	            int curDiff = maxVal -minVal;
+	            diff = Math.min(diff, curDiff);
+	            if(maxVal%2 ==0){
+	                set.remove(maxVal);
+	                set.add(maxVal/2);
+	            } else{
+	                // terminal state;
+	                return diff;
+	            }
+	        }
+	    }
+	}
+}
