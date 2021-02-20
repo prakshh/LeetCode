c000a4d2bb0e5771a6c92493fab63edd833f8c8d
prakshh <prakashdas18@gmail.com>
2021-02-20 20:56:38 +0530

    Day 9

--- _2021/_02_february2021/Day_9_538_Convert_BST_to_Greater_Tree.java
+++ _2021/_02_february2021/Day_9_538_Convert_BST_to_Greater_Tree.java
@@ -1,4 +1,35 @@
 
 public class Day_9_538_Convert_BST_to_Greater_Tree {
+	
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 *     int val;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode() {}
+	 *     TreeNode(int val) { this.val = val; }
+	 *     TreeNode(int val, TreeNode left, TreeNode right) {
+	 *         this.val = val;
+	 *         this.left = left;
+	 *         this.right = right;
+	 *     }
+	 * }
+	 */
+	class Solution {
+	    
+	    Integer greaterNodeSum = 0;
+	    
+	    public TreeNode convertBST(TreeNode root) {
+	        if(root==null){
+	            return null;
+	        }
+	        convertBST(root.right);
+	        root.val = root.val + greaterNodeSum;
+	        greaterNodeSum = root.val;
+	        convertBST(root.left);
+	        return root;
+	    }
+	}
 
 }
