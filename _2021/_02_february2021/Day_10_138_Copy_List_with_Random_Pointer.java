408a41645f29c765b28ddb16c7f0c4d5464031ee
prakshh <prakashdas18@gmail.com>
2021-02-21 18:45:24 +0530

    Day 10 new

--- _2021/_02_february2021/Day_10_138_Copy_List_with_Random_Pointer.java
+++ _2021/_02_february2021/Day_10_138_Copy_List_with_Random_Pointer.java
@@ -1,34 +1,71 @@
 
 public class Day_10_138_Copy_List_with_Random_Pointer {
-	"""
-	# Definition for a Node.
-	class Node:
-	    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-	        self.val = int(x)
-	        self.next = next
-	        self.random = random
-	"""
+	
+	/*
+	// Definition for a Node.
+	class Node {
+	    int val;
+	    Node next;
+	    Node random;
 
-	class Solution:
-	    def copyRandomList(self, head: 'Node') -> 'Node':
-	        if not head:
-	            return None
+	    public Node(int val) {
+	        this.val = val;
+	        this.next = null;
+	        this.random = null;
+	    }
+	}
+	*/
 
-	        mapping = {}
+	class Solution {
+	    public Node copyRandomList(Node head) {
 	        
-	        current = head
+	        if(head == null){
+	            return null;
+	        }
+
+	        // duplicate nodes
+	        Node it = head;
+	        while(it!=null){
+	            Node nextIt = it.next;
+	            Node newNode = new Node(it.val);
+	            newNode.next = it.next;
+	            it.next = newNode;
+	            it = nextIt;
+	        }
+
+	        // set random pointer
+
+	        Node newIt = head.next;
+	        Node newHead = head.next;
+	        it = head;
+
+	        while(it!=null){
+	            if(it.random!=null) {
+	                newIt.random = it.random.next;
+
+	            }
+	            it = newIt.next;
+	            if(it!=null)
+	                newIt = it.next;
+	        }
+
+	        // Reseting the next nodes so as to separte old and  duplicate lists
+
+	        it = head;
+	        newIt = head.next;
+
+	        while(it!=null){
+	            it.next = newIt.next;
+	            it = it.next;
+	            if(it!=null){
+	                newIt.next = it.next;
+	                newIt = it.next;
+	            }
+	        }
+
+	        return newHead;
 	        
-	        while current != None:
-	            mapping[current] = Node(current.val)    
-	            current= current.next
-
-	        for node in mapping:
-	            if node.next:
-	                mapping[node].next = mapping[node.next]
-	            if node.random:
-	                mapping[node].random = mapping[node.random]
-	            
-	            
-	        return mapping[head]
-
+	    }
+	}
+	
 }
