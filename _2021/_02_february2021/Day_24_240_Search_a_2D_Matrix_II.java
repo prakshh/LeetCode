5b66795fd8c444f74fbd20d3a06b658eafd47bad
prakshh <prakashdas18@gmail.com>
2021-02-23 15:06:24 +0530

    Day 24

---
+++ _2021/_02_february2021/Day_24_240_Search_a_2D_Matrix_II.java
@@ -0,0 +1,26 @@
+
+public class Day_24_240_Search_a_2D_Matrix_II {
+	
+	class Solution {
+	    public boolean searchMatrix(int[][] matrix, int target) {
+	        int m=matrix.length-1;
+	    int n=matrix[0].length-1;
+	 
+	    int i=m; 
+	    int j=0;
+	 
+	    while(i>=0 && j<=n){
+	        if(target < matrix[i][j]){
+	            i--;
+	        }else if(target > matrix[i][j]){
+	            j++;
+	        }else{
+	            return true;
+	        }
+	    }
+	 
+	    return false;
+	    }
+	}
+
+}
