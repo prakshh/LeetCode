c916f8538c8916a38c3528d5e9caf8f6dbdfa26d
prakshh <prakashdas18@gmail.com>
2021-02-26 16:33:39 +0530

    Day 26

---
+++ _2021/_02_february2021/Day_26_946_Validate_Stack_Sequences.java
@@ -0,0 +1,22 @@
+
+public class Day_26_946_Validate_Stack_Sequences {
+	
+	class Solution {
+	    
+	    // TC : O(n)
+	    // SC : O(n)
+	    public boolean validateStackSequences(int[] pushed, int[] popped) {
+	        Stack<Integer> st= new Stack<>();
+	        int s = 0;
+	        for(int i=0;i<pushed.length;i++){
+	            st.push(pushed[i]);
+	            while(!st.empty() && popped[s] ==st.peek()){
+	                st.pop();
+	                s++;
+	            }
+	        }
+	        return st.empty();
+	    }
+	}
+
+}
