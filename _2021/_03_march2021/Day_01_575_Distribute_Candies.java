784385c7a22f09b18942c717af686e42a1d7e101
prakshh <prakashdas18@gmail.com>
2021-03-02 16:12:29 +0530

    Day 1 March2021

---
+++ _2021/_03_march2021/Day_01_575_Distribute_Candies.java
@@ -0,0 +1,17 @@
+
+public class Day_01_575_Distribute_Candies {
+	
+	class Solution {
+	    // TC : O(n)
+	    // SC : O(n) // 1 2 3 4 6 ...n
+	     public int distributeCandies(int[] candyType) {
+
+	         HashSet<Integer> hashSet = new HashSet<>();
+	         for(int type : candyType){
+	             hashSet.add(type);
+	         }
+
+	         return Math.min(hashSet.size(), candyType.length/2);
+	     }
+
+}
