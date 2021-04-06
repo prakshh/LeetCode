9c8733fe4d634db0a115b75ac5493de3f7145687
prakshh <prakashdas18@gmail.com>
2021-04-06 23:21:05 +0530

    Day 06

---
+++ _2021/_04_april2021/Day_06_1551_Minimum_Operations_to_Make_Array_Equal.java
@@ -0,0 +1,20 @@
+
+public class Day_06_1551_Minimum_Operations_to_Make_Array_Equal {
+	
+	class Solution {
+	    // TC : O(1)
+	    // TC : O(1)
+	    public int minOperations(int n) {
+	        if(n%2==1){
+	            // n is odd
+	            n =n /2;
+	            return n*(n+1);
+	        } else{
+	            // n is even
+	            n =n/2;
+	            return n*n;
+	        }
+	    }
+	}
+
+}
