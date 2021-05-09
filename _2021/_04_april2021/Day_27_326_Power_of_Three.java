c627e2973bb02468532b9225bf6f041976fd0418
prakshh <prakashdas18@gmail.com>
2021-05-09 16:58:07 +0530

    Day 27

---
+++ _2021/_04_april2021/Day_27_326_Power_of_Three.java
@@ -0,0 +1,19 @@
+
+public class Day_27_326_Power_of_Three {
+	
+	class Solution {
+	    public boolean isPowerOfThree(int n) {
+	        if(n==0) 
+	            return false;
+
+	        if(n==1) 
+	            return true;
+
+	        if(n>1) 
+	            return n%3==0 && isPowerOfThree(n/3);
+	        else
+	            return false;
+	        }
+	}
+
+}
