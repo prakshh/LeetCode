357cee5aae392d8965d0f007af0fef5ad3a28451
prakshh <prakashdas18@gmail.com>
2021-05-09 16:18:09 +0530

    Day 16

---
+++ _2021/_04_april2021/Day_16_1209_Remove_All_Adjacent_Duplicates_in_String_II.java
@@ -0,0 +1,40 @@
+
+public class Day_16_1209_Remove_All_Adjacent_Duplicates_in_String_II {
+	
+	class Solution {
+
+	    // Tc: O(n)
+	    // Sc:  O(n)
+	    public String removeDuplicates(String s, int k) {
+	        Stack<Pair<Character, Integer>> st = new Stack<>();
+	        for(char c : s.toCharArray()){
+	            if(st.isEmpty() || st.peek().getKey() !=c){
+	                st.push(new Pair<>(c, 1));
+	            } else{
+	                // matching case
+	                Pair<Character, Integer> head = st.pop();
+	                st.push(new Pair<>(head.getKey(), head.getValue()+1));
+
+	                if(st.peek().getValue() == k){
+	                    st.pop();
+	                }
+	            }
+	        }
+
+	        String ans =  "";
+	        while(!st.isEmpty()){
+	            Pair<Character, Integer> head = st.pop();
+	            int freq = head.getValue();
+	            while(freq-->0){
+	                ans = head.getKey() +ans;
+	            }
+	        }
+
+	        return ans;
+	    }
+	}
+	        
+	    
+
+
+}
