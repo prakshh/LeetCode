894f7b6ca3573fa18f69ad76651f9e1ba7d2e892
prakshh <prakashdas18@gmail.com>
2021-05-17 19:10:20 +0530

    Day 17

---
+++ _2021/_05_may2021/Day_17_1048_Longest_String_Chain.java
@@ -0,0 +1,25 @@
+
+public class Day_17_1048_Longest_String_Chain {
+	
+	class Solution {
+		  public int longestStrChain(String[] words) {
+		    int ans = 0;
+		    Map<String, Integer> dp = new HashMap<>();
+
+		    Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+		    for (final String word : words) {
+		      int bestLength = 0;
+		      for (int i = 0; i < word.length(); ++i) {
+		        String pred = word.substring(0, i) + word.substring(i + 1);
+		        bestLength = Math.max(bestLength, dp.getOrDefault(pred, 0) + 1);
+		      }
+		      dp.put(word, bestLength);
+		      ans = Math.max(ans, bestLength);
+		    }
+
+		    return ans;
+		  }
+		}
+
+}
