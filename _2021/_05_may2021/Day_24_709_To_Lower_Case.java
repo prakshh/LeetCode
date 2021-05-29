573e09df8971163243bbb382b37bc6340ac84095
prakshh <prakashdas18@gmail.com>
2021-05-29 14:56:13 +0530

    Day 24

---
+++ _2021/_05_may2021/Day_24_709_To_Lower_Case.java
@@ -0,0 +1,24 @@
+
+public class Day_24_709_To_Lower_Case {
+	
+	class Solution {
+
+	    // TC : O(n)
+	    public String toLowerCase(String s) {
+	        char[] cList = s.toCharArray();
+	        for(int i=0;i<cList.length;i++) {
+
+	            // valid within range of upper case chars
+	            if(cList[i]>= 'A' && cList[i]<='Z')  {
+
+	                cList[i] = (char)( cList[i] +32); // lower case
+
+	            }
+	        }
+	        return new String(cList);
+	    }
+	}
+	        
+
+
+}
