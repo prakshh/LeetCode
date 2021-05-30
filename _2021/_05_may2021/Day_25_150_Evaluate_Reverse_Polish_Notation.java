9e711dca7bb9740b4e021195ea31523793a69539
prakshh <prakashdas18@gmail.com>
2021-05-30 15:04:07 +0530

    Day 25

--- _2021/_05_may2021/Day_25_150_Evaluate_Reverse_Polish_Notation.java
+++ _2021/_05_may2021/Day_25_150_Evaluate_Reverse_Polish_Notation.java
@@ -1,4 +1,48 @@
 
 public class Day_25_150_Evaluate_Reverse_Polish_Notation {
+	
+	class Solution {
+	    // TC  : O(n)
+
+	    public int evalRPN(String[] tokens) {
+	        Stack<String> st = new Stack<>();
+
+	        for(String el: tokens){
+	            if(isOperator(el)){
+
+	                int el2 = Integer.parseInt(st.pop());
+	                int el1 = Integer.parseInt(st.pop());
+	                int ans = 0;
+
+	                if(el.equals("*")){
+	                    ans = el1 *el2;
+	                } else if(el.equals("/")){
+	                    ans = el1/el2;
+	                }else if(el.equals("+")){
+	                    ans = el1 +el2;
+	                }else if(el.equals("-")){
+	                    ans = el1 -el2;
+	                }
+	                st.push(ans+"");
+	            } else {
+	                // operand
+	                st.push(el);
+	            }
+	        }
+
+	        return Integer.parseInt(st.peek());
+	    }
+
+	    private boolean isOperator(String el){
+	        if(el.equals("*") || el.equals("+") || el.equals("-") || el.equals("/")){
+	            return true;
+	        } else {
+	            return false;
+	        }
+	    }
+	}
+	        
+	    
+
 
 }
