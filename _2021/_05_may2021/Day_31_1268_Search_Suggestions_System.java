4c3050769865a82f251e1f190e4f99194116d389
prakshh <prakashdas18@gmail.com>
2021-06-01 19:55:28 +0530

    Day 31

---
+++ _2021/_05_may2021/Day_31_1268_Search_Suggestions_System.java
@@ -0,0 +1,35 @@
+
+public class Day_31_1268_Search_Suggestions_System {
+	
+	class Solution {
+	    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+	        List<List<String>> ans = new ArrayList<>();
+	        TreeMap<String, Integer> map = new TreeMap<>();
+	        Arrays.sort(products);
+
+	        List<String> productsList = Arrays.asList(products);
+
+	        for (int i = 0; i < products.length; i++) {
+	            map.put(products[i], i);
+	        }
+
+	        String key = "";
+	        for (char c : searchWord.toCharArray()) {
+	            key += c;
+	            String ceiling = map.ceilingKey(key);
+	            String floor = map.floorKey(key + "{");
+
+	            if (ceiling == null || floor == null) break;
+
+	            ans.add(productsList.subList(map.get(ceiling), Math.min(map.get(ceiling) + 3, map.get(floor) + 1)));
+	        }
+
+	        while (ans.size() < searchWord.length()) ans.add(new ArrayList<>());
+	        return ans;
+	    }
+	}
+	        
+	    
+
+
+}
