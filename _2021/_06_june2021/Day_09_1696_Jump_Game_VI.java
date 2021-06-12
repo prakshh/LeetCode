80f7b5a47873281f95df4bff83ec2e8da6a8bd3b
prakash <prakashdas18@gmail.com>
2021-06-12 22:22:21 +0530

    Day 09

---
+++ _2021/_06_june2021/Day_09_1696_Jump_Game_VI.java
@@ -0,0 +1,28 @@
+
+public class Day_09_1696_Jump_Game_VI {
+	
+	class Solution {
+	    // TC : O(nlogk)
+
+	    public int maxResult(int[] nums, int k) {
+	        if(nums.length == 0) return 0;
+	        // Score, Index
+	        Queue<int[]> pq = new PriorityQueue<>((a, b) -> (b[0] - a[0]));
+
+	        pq.offer(new int[]{nums[0], 0});
+
+	        int maxScore = nums[0];
+
+	        for(int i = 1; i<nums.length; i++) {
+	            while(!(i- pq.peek()[1] <= k)) {
+	                pq.poll();
+	            }
+	            int[] cur = pq.peek();
+	            maxScore  = cur[0] + nums[i];
+	            pq.offer(new int[]{maxScore, i});
+	        }
+	        return maxScore;
+	    }
+	}
+
+}
