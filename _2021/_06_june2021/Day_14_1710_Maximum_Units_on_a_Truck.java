d872e859da1479824b3d8c17a8dabf089f4d812e
prakash <prakashdas18@gmail.com>
2021-06-17 20:22:51 +0530

    Day 14

---
+++ _2021/_06_june2021/Day_14_1710_Maximum_Units_on_a_Truck.java
@@ -0,0 +1,28 @@
+
+public class Day_14_1710_Maximum_Units_on_a_Truck {
+	
+	class Solution {
+
+	    // TC : O(nlogn)
+	    // SC : O(1)
+	    public int maximumUnits(int[][] boxTypes, int truckSize) {
+	        // Sort on the basis of unit in the box in decreasing order
+	        Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+	        int maxUnitOfboxes = 0;
+	        for (int[] box : boxTypes) {
+	            int noOfBoxes = box[0];
+	            int noOfUnits= box[1];
+
+	            if (truckSize >= noOfBoxes) {
+	                maxUnitOfboxes += noOfBoxes * noOfUnits;
+	                truckSize -= noOfBoxes;
+	            }else {
+	                maxUnitOfboxes += truckSize * noOfUnits;
+	                return maxUnitOfboxes;
+	            }
+	        }
+	        return maxUnitOfboxes;
+	    }
+	}
+
+}
