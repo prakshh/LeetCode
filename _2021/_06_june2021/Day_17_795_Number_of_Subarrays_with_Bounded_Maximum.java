6b66bcd72c27649c8f19de869886d910b74529b9
prakash <prakashdas18@gmail.com>
2021-06-20 17:27:07 +0530

    Day 17

---
+++ _2021/_06_june2021/Day_17_795_Number_of_Subarrays_with_Bounded_Maximum.java
@@ -0,0 +1,39 @@
+
+public class Day_17_795_Number_of_Subarrays_with_Bounded_Maximum {
+	
+	class Solution {
+	    // TC : O(n)
+	    // SC : O(1)
+	    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+	        int res = 0;
+	        int start =0;
+	        int end = 0;
+	        int windowCount = 0;
+	        while(end<nums.length){
+
+	            // el within the range recalculate the window
+	            if(nums[end]>=left && nums[end]<=right) {
+
+	                windowCount = end-start+1;
+	            }
+	            // el outside the right range, update start  + reset window
+	            else if(nums[end]>right){
+	                windowCount = 0;
+	                start = end +1;
+	            } else  if(nums[end] < left){
+	                // el less than range use previous window
+	                windowCount = windowCount;
+	            }
+
+
+	            res +=  windowCount;
+	            end++;
+	        }
+
+
+	        return res;
+
+	    }
+	}
+
+}
