67dc7b67dd8d1d014c8ba50df0c05d75467f4881
prakash <prakashdas18@gmail.com>
2021-07-12 22:15:48 +0530

    Day 09

---
+++ _2021/_07_july2021/Day_09_300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+  public int lengthOfLIS(int[] nums) {
+    TreeSet<Integer> bst = new TreeSet<>();
+    for (int num : nums) {
+      Integer higherOrEqual = bst.ceiling(num);
+      if (higherOrEqual == null) {
+        bst.add(num);
+      }
+      else {
+        bst.remove(higherOrEqual);
+        bst.add(num); // O(logn)
+      }
+    }
+
+    return bst.size();
+  }
+    
+}
