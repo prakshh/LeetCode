630db77653be62b5cecf0186c2e2274eaa26eaeb
prakash <prakashdas18@gmail.com>
2021-07-19 19:31:08 +0530

    Day 15

---
+++ _2021/_07_july2021/Day_15_611_Valid_Triangle_Number.java
@@ -0,0 +1,31 @@
+
+public class Day_15_611_Valid_Triangle_Number {
+	
+	class Solution {
+
+		  // TC : O(n2)
+		  // SC : O(1)
+		  public int triangleNumber(int[] nums) {
+		    int ans  = 0;
+		    if (nums.length < 3) return ans;
+
+		    Arrays.sort(nums);
+
+		    for (int i = 2; i < nums.length; i++) {
+		      int left = 0, right = i - 1;
+		      while (left < right) {
+		        if (nums[left] + nums[right] > nums[i]) {
+		          ans += (right - left);
+		          right--;
+		        }
+		        else {
+		          left++;
+		        }
+		      }
+		    }
+
+		    return ans;
+		  }
+		}
+
+}
