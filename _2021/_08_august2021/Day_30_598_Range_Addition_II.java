4d15f8869cd37500b171423dbb4947b97d9c2a5f
“prakshh” <prakashdas18@gmail.com>
2021-09-01 18:59:48 +0530

    Day 30

---
+++ _2021/_08_august2021/Day_30_598_Range_Addition_II.java
@@ -0,0 +1,18 @@
+public class Day_30_598_Range_Addition_II {
+
+    class Solution {
+        // TC : O(No of Ops (ops.length))
+        public int maxCount(int m, int n, int[][] ops) {
+
+            int minRow = m, minCol = n;
+            for (int[] op : ops) {
+                minRow = Math.min(minRow, op[0]);
+                minCol = Math.min(minCol, op[1]);
+            }
+
+            return minCol * minRow;
+
+        }
+    }
+
+}
