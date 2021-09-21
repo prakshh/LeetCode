ba60eb61f31db50c53c3ed628a4ac0921dd54c4d
“prakshh” <prakashdas18@gmail.com>
2021-09-21 07:33:13 +0530

    Day 14

---
+++ _2021/_09_september2021/Day_14_917_Reverse_Only_Letters.java
@@ -0,0 +1,36 @@
+public class Day_14_917_Reverse_Only_Letters {
+
+    class Solution{
+        // TC: O(n)
+        // SC : O(1)
+        public String reverseOnlyLetters(String s) {
+            char[] c = s.toCharArray();
+            int start = 0;
+            int end = s.length()-1;
+
+            while(start<end){
+                if(!isEnglishCharacter(c[start])){
+                    start++;
+                    continue;
+                }
+                if(!isEnglishCharacter(c[end])){
+                    end--;
+                    continue;
+                }
+
+                // swap
+                char temp = c[start];
+                c[start] = c[end];
+                c[end] = temp;
+                start++;
+                end--;
+            }
+            return new String(c);
+        }
+
+        private boolean isEnglishCharacter(char c){
+            return Character.isUpperCase(c) || Character.isLowerCase(c);
+        }
+    }
+
+}
