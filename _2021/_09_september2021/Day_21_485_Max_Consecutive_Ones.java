e330f14312abc2ff20f9b595534f6fc3eae2a88d
“prakshh” <prakashdas18@gmail.com>
2021-10-02 01:12:35 +0530

    Day 21

---
+++ _2021/_09_september2021/Day_21_485_Max_Consecutive_Ones.java
@@ -0,0 +1,16 @@
+public class Day_21_485_Max_Consecutive_Ones {
+    class Solution {
+        public int findMaxConsecutiveOnes(int[] nums) {
+
+            int mxCount = nums[0];
+            for(int i=1;i<nums.length;i++){
+                if(nums[i]==1)  {
+                    nums[i] += nums[i-1];
+                }
+                mxCount = Math.max(mxCount, nums[i]);
+            }
+
+            return mxCount;
+        }
+    }
+}
