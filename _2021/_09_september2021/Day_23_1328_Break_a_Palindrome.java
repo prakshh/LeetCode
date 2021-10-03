4dc6b8fea4618ba9091141239d654ae073ee1731
“prakshh” <prakashdas18@gmail.com>
2021-10-03 21:10:18 +0530

    Day 23

---
+++ _2021/_09_september2021/Day_23_1328_Break_a_Palindrome.java
@@ -0,0 +1,25 @@
+public class Day_23_1328_Break_a_Palindrome {
+    class Solution {
+
+
+        public String breakPalindrome(String palindrome) {
+            char[] cStr = palindrome.toCharArray();
+            // a
+            if(cStr.length<2){
+                return "";
+            }
+
+
+            for (int i = 0; i <cStr.length / 2; i++) {
+                if (cStr[i] != 'a') {
+                    cStr[i] = 'a';
+                    return String.valueOf(cStr);
+                }
+            }
+            // crossed middle point all ones
+            cStr[cStr.length - 1] = 'b'; //if all 'a'
+            return String.valueOf(cStr);
+        }
+    }
+
+}
