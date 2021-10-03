c545fd677b87278caa6d207c531c500ad604e565
“prakshh” <prakashdas18@gmail.com>
2021-10-03 19:42:46 +0530

    Day 22

---
+++ _2021/_09_september2021/Day_22_1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters.java
@@ -0,0 +1,35 @@
+public class Day_22_1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters {
+
+    class Solution {
+        private boolean isUnique(String str) {
+            if (str.length() > 26) return false;
+            boolean[] used = new  boolean [26];
+            char[] arr = str.toCharArray();
+            for (char ch : arr) {
+                if (used[ch - 'a']){
+                    return false;
+                } else {
+                    used[ch -'a'] = true;
+                }
+            }
+            return true;
+        }
+        public int maxLength(List<String> arr) {
+            List<String> res = new ArrayList<>();
+            res.add("");
+            for (String str : arr) {
+                if (!isUnique(str)) continue;
+                List<String> resList = new ArrayList<>();
+                for (String candidate : res) {
+                    String temp = candidate + str;
+                    if (isUnique(temp)) resList.add(temp);
+                }
+                res.addAll(resList);
+            }
+            int ans = 0;
+            for (String str : res) ans = Math.max(ans, str.length());
+            return ans;
+        }
+    }
+
+}
