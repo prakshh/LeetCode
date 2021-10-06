a2cb67a2d7840041fdc5981be920033d16382a1f
“prakshh” <prakashdas18@gmail.com>
2021-10-06 09:42:37 +0530

    Day 28

---
+++ _2021/_09_september2021/Day_28_922_Sort_Array_By_Parity_II.java
@@ -0,0 +1,31 @@
+public class Day_28_922_Sort_Array_By_Parity_II {
+    class Solution {
+        // TC : O(n)
+        // SC : O(1)
+        public int[] sortArrayByParityII(int[] nums) {
+            int oddNoAtEvenIndex= 0;
+            int evenNoAtOddIndex = 1;
+            int n = nums.length;
+            while (oddNoAtEvenIndex < n && evenNoAtOddIndex < n) {
+                while (oddNoAtEvenIndex < n && nums[oddNoAtEvenIndex] % 2 == 0) {
+                    oddNoAtEvenIndex += 2;
+                }
+                while (evenNoAtOddIndex < n && nums[evenNoAtOddIndex] % 2 == 1) {
+                    evenNoAtOddIndex += 2;
+                }
+                if (evenNoAtOddIndex < n && oddNoAtEvenIndex < n) {
+                    swap(nums, oddNoAtEvenIndex, evenNoAtOddIndex);
+                }
+            }
+            return nums;
+        }
+        private void swap(int[] nums, int i, int j) {
+            if(i!=j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+
+    }
+}
