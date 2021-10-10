4ff99fb2f3d139cdf7ef1b63e11480a70b7972e2
“prakshh” <prakashdas18@gmail.com>
2021-10-10 17:10:44 +0530

    Day 03

---
+++ _2021/_10_october2021/Day_03_55_Jump_Game.java
@@ -0,0 +1,21 @@
+public class Day_03_55_Jump_Game {
+    class Solution {
+        public boolean canJump(int[] nums) {
+            if(nums.length==1){
+                return true;
+            }
+            int iMaxReach = nums[0];
+            int i = 1;
+            while(iMaxReach>=i) {
+                if(iMaxReach>=nums.length-1){
+                    return true;
+                }
+                iMaxReach = Math.max(nums[i] +i , iMaxReach);
+                i++;
+
+            }
+            return false;
+        }
+    }
+
+}
