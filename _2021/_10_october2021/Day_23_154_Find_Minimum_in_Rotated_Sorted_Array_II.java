d85d64142a7ea0699676cc409f9f2c6e49a879ae
“prakshh” <prakashdas18@gmail.com>
2021-10-31 19:35:29 +0530

    Day 23

---
+++ _2021/_10_october2021/Day_23_154_Find_Minimum_in_Rotated_Sorted_Array_II.java
@@ -0,0 +1,29 @@
+public class Day_23_154_Find_Minimum_in_Rotated_Sorted_Array_II {
+    class Solution {
+        // TC : O(n)
+        // SC : O(1)
+        public int findMin(int[] nums) {
+            int low = 0;
+            int high = nums.length - 1;
+            while(low < high){
+                int mid = low + ( high - low)/2;
+                if(nums[high] == nums[mid]){
+                    high = high -1;
+                }
+                else if(nums[high] > nums[mid]){
+                    high = mid;
+                } else{
+                    // nums[high] < nums[mid]
+                    low = mid+1;
+
+                }
+            }
+            return nums[low];
+        }
+    }
+
+
+
+
+
+}
