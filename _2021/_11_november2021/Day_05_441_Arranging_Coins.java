781ed8cd63b5a65ff786c050d0637f8a66a251c7
“prakshh” <prakashdas18@gmail.com>
2021-11-13 22:35:27 +0530

    Day 05

---
+++ _2021/_11_november2021/Day_05_441_Arranging_Coins.java
@@ -0,0 +1,15 @@
+public class Day_05_441_Arranging_Coins {
+    public class Solution {
+        public int arrangeCoins(int n) {
+            if(n<0) return -1;
+            if(n==0) return 0;
+            int rows = 0;
+            long sum = 0;
+            while(sum<=n){
+                sum += rows+1;
+                rows++;
+            }
+            return rows-1;
+        }
+    }
+}
