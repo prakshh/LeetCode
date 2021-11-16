c9d14056e67f030a1b7a9f668c028f03228be78d
“prakshh” <prakashdas18@gmail.com>
2021-11-16 22:28:26 +0530

    Day 08

---
+++ _2021/_11_november2021/Day_08_96_Unique_Binary_Search_Trees.java
@@ -0,0 +1,16 @@
+public class Day_08_96_Unique_Binary_Search_Trees {
+    class Solution {
+        // TC : O(n2)
+        private int[] dp = new int[20];
+        public int numTrees(int n) {
+
+            if(n <= 1) return 1;
+            if(dp[n]>0) return dp[n];
+            for(int i = 1; i <= n; i++)
+                dp[n] += numTrees(i-1) * numTrees(n-i);
+            return dp[n];
+
+        }
+
+    }
+}
