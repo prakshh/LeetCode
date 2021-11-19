2be4df0ff540fe742689c08ea5dcd0eadc5e0a9c
“prakshh” <prakashdas18@gmail.com>
2021-11-19 23:24:32 +0530

    Day 10

---
+++ _2021/_11_november2021/Day_10_122_Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -0,0 +1,40 @@
+public class Day_10_122_Best_Time_to_Buy_and_Sell_Stock_II {
+    class Solution {
+
+        // TC : O(n)
+        // SC : O(1)
+        public int maxProfit(int[] prices) {
+            int i = 0;
+            int buy =0;
+            int sell = 0;
+            int profit = 0;
+            int n = prices.length - 1;
+            while (i < n) {
+                // till the time prices are decreasing
+                while (i < n && prices[i + 1] <= prices[i])  {
+                    i++;
+                }
+                // found the buying price
+                buy = prices[i];
+                //  System.out.print(" Buy " + buy);
+                // till the time prices are increasing
+                while (i < n && prices[i + 1] > prices[i]) {
+                    i++;
+                }
+
+                // found the selling price
+                sell = prices[i];
+
+
+                // System.out.print(" Sell " + sell);
+
+                profit += sell - buy;
+            }
+            return profit;
+        }
+    }
+
+
+
+
+}
