dcd43e103c3895c5552bb3cf2c8c41ffb03fd83e
“prakshh” <prakashdas18@gmail.com>
2021-11-29 22:40:01 +0530

    Day 13

---
+++ _2021/_11_november2021/Day_13_739_Daily_Temperatures.java
@@ -0,0 +1,26 @@
+public class Day_13_739_Daily_Temperatures {
+    class Solution {
+        public int[] dailyTemperatures(int[] temperatures) {
+            int len = temperatures.length;
+            int[] ans = new int[len];
+
+            Stack<Integer> st = new Stack<>();
+
+            for(int i=0;i<len;i++){
+
+                while(!st.empty() && temperatures[st.peek()] <temperatures[i]){
+                    //System.out.println(temperatures[st.peek()]);
+                    // found an increasing value for st.peek() index
+                    ans[st.peek()] = i - st.peek();
+                    st.pop();
+                    // System.out.println( "removed "+ st.peek() + " " + temperatures[st.pop()]);
+
+                }
+
+                st.push(i);
+
+            }
+            return ans;
+        }
+    }
+}
