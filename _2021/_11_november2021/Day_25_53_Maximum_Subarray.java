c7f87bc762dc75912606246a0e0b5cc445e79271
“prakshh” <prakashdas18@gmail.com>
2021-12-05 22:54:07 +0530

    Day 25

---
+++ _2021/_11_november2021/Day_25_53_Maximum_Subarray.java
@@ -0,0 +1,15 @@
+public class Day_25_53_Maximum_Subarray {
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            int result = nums[0];
+            int sum = nums[0];
+
+            for(int i=1; i<nums.length; i++){
+                sum = Math.max(nums[i], sum + nums[i]);
+                result = Math.max(result, sum);
+            }
+
+            return result;
+        }
+    }
+}
