3716b824d429c14d760797c4a3914cb961947849
“prakshh” <prakashdas18@gmail.com>
2021-12-12 21:52:39 +0530

    Day 07

---
+++ _2021/_12_december2021/Day_07_1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -0,0 +1,26 @@
+public class Day_07_1290_Convert_Binary_Number_in_a_Linked_List_to_Integer{
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        // TC : O(n)
+        // SC : O(1)
+        public int getDecimalValue(ListNode head) {
+            int no = 0;
+            ListNode it = head;
+            while (it != null){
+                no = 2 *no+ it.val;
+
+                it = it.next;
+            }
+            return no;
+        }
+    }
+}
